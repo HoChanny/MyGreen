16e3299a4e39e6a12b4aa217ee253e78619f3e4a
ys0222 <ggdbstjd0222@gmail.com>
2023-03-14 03:28:05 +0900

    fix 커밋 버그 수정

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -4,6 +4,7 @@ class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _LoginPageState createState() => _LoginPageState();
 }
 
