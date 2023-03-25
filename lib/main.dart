7704e064019594bc5abd33137157d072dcfd9ac3
ys0222 <ggdbstjd0222@gmail.com>
2023-03-25 16:50:46 +0900

    Test : Merge

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:mygreen/create/create.dart';
 import 'package:mygreen/login/login.dart';
 import 'package:mygreen/calendar/calendar.dart';
+import 'package:mygreen/screen/pot_registration_screen.dart';
 
 Future<void> main() async {
   await initializeDateFormatting();
@@ -19,38 +20,48 @@ class Test extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.amber,
-        body: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-              ElevatedButton(
-                child: const Text('회원가입'),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const CreateAccountPage()));
-                },
-              ),
-              ElevatedButton(
-                child: const Text('로그인'),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const LoginPage()));
-                },
-              ),
-              ElevatedButton(
-                child: const Text('test'),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const CalendarPage()));
-                },
-              )
-            ])));
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              child: const Text('회원가입'),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const CreateAccountPage()));
+              },
+            ),
+            ElevatedButton(
+              child: const Text('로그인'),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()));
+              },
+            ),
+            ElevatedButton(
+              child: const Text('calendar'),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const CalendarPage()));
+              },
+            ),
+            ElevatedButton(
+              child: const Text('test'),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const RegistrationPage()));
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
