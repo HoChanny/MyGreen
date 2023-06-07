20f427998276918d813b6b2cf697413e97faed8d
HoChanny <qboooodp@naver.com>
2023-06-07 12:58:12 +0900

    Feat: push notification test version

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:mygreen/screen/login_page.dart';
 import 'package:get/get.dart';
 
 import 'package:intl/date_symbol_data_local.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print(fcmToken);
   await initializeDateFormatting();
   runApp(const GetMaterialApp(home: MyApp()));
 }
