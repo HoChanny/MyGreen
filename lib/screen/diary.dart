06976c12d22f50f0571379d0fa89518a184a3d6b
ys0222 <ggdbstjd0222@gmail.com>
2023-09-09 02:02:32 +0900

    [Design] : Diary 디자인 변경

--- lib/screen/diary.dart
+++ lib/screen/diary.dart
@@ -58,67 +58,64 @@ class _DiaryPageState extends State<DiaryPage> {
             children: [
               //날짜
               Container(
-  padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.05),
-  child: Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Container(
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.black, // Border color
-            width: 5.0, // Border width
-          ),
-          borderRadius: BorderRadius.all(Radius.circular(4.0)), // Border radius
-        ),
-        child: Image.asset(
-          'assets/image/test.png',
-          width: 150,
-          height: 150,
-        ),
-      ),
-    ],
-  ),
-)
-,
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.05),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: Image.asset(
+                        'assets/image/test.png',
+                        width: MediaQuery.of(context).size.height * 0.4,
+                        height: MediaQuery.of(context).size.height * 0.3,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
 
-              
-              //날짜 & 이모지
+              // 날짜 & 이모지를 포함한 컨테이너
               Container(
-  width: MediaQuery.of(context).size.width * 0.8,
-  height: 100,
-  padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.05),
-  child: Row(
-    children: [
-      Text(
-        ' ${month}월 ${day}일 ',
-        style: const TextStyle(
-          fontSize: 25,
-          fontWeight: FontWeight.w800,
-          color: Colors.black54,
-          fontFamily: '',
-        ),
-      ),
-      DiaryEmotion(emotion: widget.emotion),
-    ],
-  ),
-),
-
-
-              // 내용
-              Container(
-                width: MediaQuery.of(context).size.width *
-                    0.8, // Set as a fraction of the screen width
+                width: MediaQuery.of(context).size.width * 0.7,
                 height: 100,
+                padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.05,
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      ' ${month}월 ${day}일 ',
+                      style: const TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w700,
+                        color: Color.fromARGB(255, 14, 14, 14),
+                        fontFamily: '',
+                      ),
+                    ),
+                    DiaryEmotion(emotion: widget.emotion),
+                  ],
+                ),
+              ),
 
-                child: Text(
-                  '${widget.content}',
-                  style: const TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
-                    fontFamily: '',
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(87, 211, 210, 210), // 배경색을 원하는 색상으로 설정
+                  borderRadius: BorderRadius.circular(10.0), // 원하는 반지름으로 조절
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0), // 필요에 따라 패딩 값을 조정
+                  child: Text(
+                    '${widget.content}',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontFamily: '',
+                    ),
                   ),
                 ),
               ),
+
               //감정
             ],
           ),
